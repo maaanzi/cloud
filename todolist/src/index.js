35fcf53d3f7175d1c9d8e079b931e579e55d8a95
maaanzi <mz151@naver.com>
2022-12-19 22:32:31 +0900

    주석추가

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const { image } = require("./upload");
 
-app.set("view engine", "html");
+app.set("view engine", "html"); // node에 html을 랜더링 하기 위해 뷰엔진 사용
 app.set("views", process.cwd() + "/src/views");
 nunjucks.configure("src/views", {
   autoescape: true,
@@ -17,10 +17,11 @@ app.listen(3000, function () {
   console.log("3000번 포트 대기중~");
 });
 
-app.use("/css", express.static(path.join(process.cwd(), "src/public")));
+app.use("/css", express.static(path.join(process.cwd(), "src/public"))); // css 연결
 
 app.use("/upload", express.static("upload"));
 app.post("/", image.single("image"), (req, res) => {
+  // 이미지 받아오기
   console.log(req.file);
   const img = req.file.location;
   return res.render(path.join(process.cwd(), "src/views/index.html"), { img });
--- todolist/src/upload.js
+++ todolist/src/upload.js
@@ -8,6 +8,7 @@ module.exports.localsMiddleware = (req, res, next) => {
 };
 
 const s3 = new aws.S3({
+  // iam 아이디와 비밀번호
   credentials: {
     accessKeyId: "AKIA44XT6DHIIFOFDBMY",
     secretAccessKey: "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",
@@ -15,12 +16,14 @@ const s3 = new aws.S3({
 });
 
 const s3Image = multerS3({
+  // s3 버킷에 저장
   s3: s3,
   bucket: "todooo",
   acl: "public-read",
 });
 
 module.exports.image = multer({
+  // 이미지 업로드
   dest: "upload/images/",
   limits: {
     fileSize: 10000000,
